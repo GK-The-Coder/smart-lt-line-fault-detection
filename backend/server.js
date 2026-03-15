1e209cefd72d0cf656367e43889e131b74f12634
Ganesh Karadkar <karadkarganesh797@gmail.com>
2026-03-15 10:58:56 +0530

    Configure CORS to allow all origins

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(cors({
+  origin: "*"
+}));
+
 mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB Connected"));
 
